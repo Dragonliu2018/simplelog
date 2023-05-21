368f378cd5dc04e64fcdb6b511bffaf7874eb14d
刘振龙 <dragonliu@buaa.edu.cn>
2023-05-22 00:07:43 +0800

    子模块名称

--- dlplog.h
+++ dlplog.h
@@ -9,6 +9,14 @@ typedef enum {
     ERROR
 } LogLevel;
 
+// TODO: 补充子模块名称
+// 子模块名称
+typedef enum {
+    Sampling_Node, // 采集节点
+    Analysis_Node, // 分析节点
+    Management_Node, // 管理节点
+} SubmoduleName;
+
 // 获得当前时间
 const char *get_timestamp()
 {
@@ -39,43 +47,70 @@ const char *get_log_level_str(LogLevel level)
             levelString = "ERROR";
             break;
         default:
-            levelString = "UNKNOWN";
+            levelString = "UNKNOWN LogLevel";
             break;
     }
 
     return levelString;
 }
 
+// 通过SubmoduleName获得字符串
+const char *get_submodule_name_str(SubmoduleName name)
+{
+    // 根据子模块选择输出格式
+    const char* nameString;
+
+    switch (name) {
+        case Sampling_Node:
+            nameString = "Sampling Node";
+            break;
+        case Analysis_Node:
+            nameString = "Analysis Node";
+            break;
+        case Management_Node:
+            nameString = "Management Node";
+            break;
+        default:
+            nameString = "UNKNOWN SubmoduleName";
+            break;
+    }
+
+    return nameString;
+}
+
 // 日志输出
-void LOG(LogLevel level, const char *message)
+void LOG(SubmoduleName submodule, LogLevel level, const char *message)
 {
     // 获取当前时间
     const char *timestamp = get_timestamp();
+
+    // 子模块名称
+    const char *submoduleString = get_submodule_name_str(submodule);
 
     // 日志等级
     const char *levelString = get_log_level_str(level);
 
     // 输出日志消息
-    printf("[%s] [%s] %s\n", timestamp, levelString, message);
+    printf("[%s] [%s] [%s] %s\n", timestamp, submoduleString, levelString, message);
 }
 
 /* 外部接口 */
-void LOG_DEBUG(const char *message)
+void LOG_DEBUG(SubmoduleName name, const char *message)
 {
-    LOG(DEBUG, message);
+    LOG(name, DEBUG, message);
 }
 
-void LOG_INFO(const char *message)
+void LOG_INFO(SubmoduleName name, const char *message)
 {
-    LOG(INFO, message);
+    LOG(name, INFO, message);
 }
 
-void LOG_WARNIGN(const char *message)
+void LOG_WARNIGN(SubmoduleName name, const char *message)
 {
-    LOG(WARNING, message);
+    LOG(name, WARNING, message);
 }
 
-void LOG_ERROR(const char *message)
+void LOG_ERROR(SubmoduleName name, const char *message)
 {
-    LOG(ERROR, message);
+    LOG(name, ERROR, message);
 }
